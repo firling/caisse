5bf31ef79ea507c6b4e5c6fcce374e2f54b75d89
Julien Anquetil <anquetil.jul@gmail.com>
2023-03-13 18:25:21 +0100

    node fetch

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "../../../lib/prisma"
+import fetch from 'node-fetch';
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
